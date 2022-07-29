a8236e546cae26d8c8768ed4e62777244032ecd9
Adan Hernandez <101598731+cikejoae@users.noreply.github.com>
2022-07-29 15:53:25 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import { createAlchemyWeb3 } from '@alch/alchemy-web3';
 export const NFTCONTRACT = "0xA690c89558a39709e1F0DdE0f8825a83d438fcE4";
-export const STAKINGCONTRACT = "0xf10641F04319F657f4A085d26599CB1f0e76874f";
+export const STAKINGCONTRACT = "0x5D333923dFD6e933248826b4Bf0f5b2d3FFfb380";
 export const polygonscanapi = "https://api.polygonscan.com/api";
 export const moralisapi = "https://deep-index.moralis.io/api/v2";
 export const nftpng = "https://ipfs.io/ipfs/QmZnG7o3UTe35xq3ffJMtApKXnY2d2Ccxwjxny3HPvop1S/";
